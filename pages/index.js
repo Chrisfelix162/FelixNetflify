b7eb3aa6ee0541f5674126387083edcb06ba2f53
Chrisfelix162 <chrisfelix162@gmail.com>
2025-02-07 16:56:41 -0800

    Fix file structure and component setup

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,17 @@
-// pages/index.js
 import Head from 'next/head'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 import FileProcessor from '../components/FileProcessor'
 
-
 export default function Home() {
   return (
-    <div className="container mx-auto px-4">
+    <div className="container">
       <Head>
         <title>File Processing Service</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="min-h-screen py-20">
+      <main>
         <Header title="File Processing Service" />
         <FileProcessor />
       </main>
