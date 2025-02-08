324a8502707a1deecb4154e88cb246d25a7af353
Chrisfelix162 <chrisfelix162@gmail.com>
2025-02-07 16:46:50 -0800

    changed configuration files

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
 // pages/index.js
 import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
-import FileProcessor from '@components/FileProcessor'
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+import FileProcessor from '../components/FileProcessor'
+
 
 export default function Home() {
   return (
